0b078ad9ee3be01060484cd62c908940e2d27ad3
Anna Kusova <kusovanna@gmail.com>
2026-01-15 19:59:23 +0200

    hw js 9

---
+++ src/js/2-form.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = { email: '', message: '' };
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (savedData) {
+    formData.email = savedData.email ?? '';
+    formData.message = savedData.message ?? '';
+
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+};
+
+form.addEventListener('input', (event) => {
+    const { name, value } = event.target;
+
+    if (name in formData) {
+        formData[name] = value.trim();
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (!formData.email || !formData.message) {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+
+    formData.email = '';
+    formData.massege = '';
+
+    form.reset();
+});
